168ee131bd3e21e9b0507c85af27cc2b33c89d2a
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2016-12-06 15:05:54 +0100

    decorate a function instead of a method

--- sklearn/pipeline.py
+++ sklearn/pipeline.py
@@ -578,7 +578,7 @@ class CachedPipeline(Pipeline):
     --------
     """
 
-    def __init__(self, steps, memory=None):
+    def __init__(self, steps, memory=Memory(cachedir=None, verbose=10)):
         self.memory = memory
 
         super(CachedPipeline, self).__init__(steps)
@@ -591,52 +591,20 @@ class CachedPipeline(Pipeline):
             step, param = pname.split('__', 1)
             fit_params_steps[step][param] = pval
         Xt = X
+        memory = self.memory
+        if isinstance(memory, six.string_types):
+            memory = Memory(cachedir=memory, verbose=10)
         for name, transform in self.steps[:-1]:
             if transform is None:
                 pass
             else:
-                Xt, transform = self._fit_transform_one(
+                Xt, transform = memory.cache(_fit_transform_one)(
                     transform, name,
                     None, Xt, y,
                     **fit_params_steps[name])
         if self._final_estimator is None:
             return Xt, {}
         return Xt, fit_params_steps[self.steps[-1][0]]
-
-    def fit(self, X, y=None, **fit_params):
-        """Fit the model
-
-        Fit all the transforms one after the other and transform the
-        data, then fit the transformed data using the final estimator.
-
-        Parameters
-        ----------
-        X : iterable
-            Training data. Must fulfill input requirements of first step of the
-            pipeline.
-
-        y : iterable, default=None
-            Training targets. Must fulfill label requirements for all steps of
-            the pipeline.
-
-        **fit_params : dict of string -> object
-            Parameters passed to the ``fit`` method of each step, where
-            each parameter name is prefixed such that parameter ``p`` for step
-            ``s`` has key ``s__p``.
-
-        Returns
-        -------
-        self : Pipeline
-            This estimator
-        """
-        memory = self.memory
-        if isinstance(memory, six.string_types) or memory is None:
-            memory = Memory(cachedir=memory, verbose=10)
-        self._fit_transform_one = memory.cache(_fit_transform_one)
-
-        super(CachedPipeline, self).fit(X, y, **fit_params)
-
-        return self
 
 
 def make_pipeline(*steps):
