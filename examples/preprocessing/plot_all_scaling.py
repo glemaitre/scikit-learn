8272ec1061bd07593080c1a3f51bcd673def65ab
Raghav RV <rvraghav93@gmail.com>
2017-02-21 17:11:24 +0100

    Better aesthetics; Better spacing and plot colormap only at end

--- examples/preprocessing/plot_all_scaling.py
+++ examples/preprocessing/plot_all_scaling.py
@@ -10,12 +10,11 @@ The feature 0 and feature 5 of california housing dataset contains large
 outliers that can make visualization of the data difficult.
 
 Also linear models like :class:`sklearn.linear_model.SVM` require data which is
-normalized to the range [-1, 1].
+approximately normalized to the [-1, 1] or [0, 1] range, or at the very least
+have all the features on the same scale.
 
 This example uses different scalers and normalizers to bring the data within a
 smaller range.
-
-# TODO make doc better
 """
 from __future__ import print_function
 print(__doc__)
@@ -44,8 +43,9 @@ dataset = fetch_california_housing()
 X_full, y_full = dataset.data, dataset.target
 
 # Take only 2 features to make visualization easier
-# Feature 0 has a tapering distribution of outliers
-# Feature 5 has a few but very large outliers
+# Feature of 0 has a long tail distribution.
+# Feature 5 has a few but very large outliers.
+
 X = X_full[:, [0, 5]]
 
 X_min_max_scaled = MinMaxScaler().fit_transform(X)
@@ -66,33 +66,35 @@ def plot_distribution(axes, X, y, hist_nbins=50, plot_title="", size=(15, 10),
     ax.set_title(plot_title, fontsize=12)
     ax.set_xlabel(X_label)
     ax.set_ylabel(y_label)
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
 
     # The scatter plot
     colors = cm.plasma_r(y)
     ax.scatter(X[:, 0], X[:, 1], alpha=0.5, marker='o', s=5, lw=0, c=colors)
 
-    # The histogram for axis 0 and axis 1
-    for hist_ax, X_feat in ((hist_X1, X[:, 1]),
-                            (hist_X0, X[:, 0])):
-        if hist_ax is hist_X0:
-            orientation = 'vertical'
-            hist_ax.set_xlim(ax.get_xlim())
-            hist_ax.invert_yaxis()
-        else:
-            orientation = 'horizontal'
-            hist_ax.set_ylim(ax.get_ylim())
+    # Removing the top and the right spine for aesthetics
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.get_xaxis().tick_bottom()
+    ax.get_yaxis().tick_left()
 
-        hist_ax.hist(X_feat, bins=hist_nbins, orientation=orientation,
-                     color='grey', ec='grey')
-        hist_ax.axis('off')
+    # Histogram for axis X1 (feature 5)
+    hist_X1.set_ylim(ax.get_ylim())
+    hist_X1.hist(X[:, 1], bins=hist_nbins, orientation='horizontal',
+                 color='grey', ec='grey')
+    hist_X1.axis('off')
 
-gs = gridspec.GridSpec(7 * 4, 2 * 2, right=4, left=3,
-                       width_ratios=[5, 1, 5, 1], wspace=0.3,
-                       height_ratios=[5, 1, 0.2, 0.5] * 7, hspace=0.5)
+    # Histogram for axis X0 (feature 0)
+    hist_X0.set_xlim(ax.get_xlim())
+    hist_X0.invert_yaxis()
+    hist_X0.hist(X[:, 0], bins=hist_nbins, orientation='vertical',
+                 color='grey', ec='grey')
+    hist_X0.axis('off')
+
+fig = plt.figure(figsize=(15, 50))
+gs = gridspec.GridSpec(15, 5,
+                       width_ratios=[5, 1, 0.1, 5, 1], wspace=0.3,
+                       height_ratios=[5, 1] * 7 + [0.4], hspace=0.4)
 subplots = list(plt.subplot(g) for g in gs)
-subplots[0].figure.set_size_inches(10, 50)
 
 for i, (X, title) in enumerate((
         (X, "Unscaled data"),
@@ -102,32 +104,33 @@ for i, (X, title) in enumerate((
         (X_standard_scaled, "Data after standard scaling"),
         (X_l2_normalized, "Data after sample-wise L2 normalizing"),
         (X_quantile_normalized, "Data after quantile normalizing"))):
-    offset = 16 * i
-    axes = subplots[offset: offset + 2] + subplots[offset + 4: offset + 6]
+    offset = 10 * i
+
+    # Distribution with all outliers
+    axes = subplots[offset:offset + 2] + subplots[offset + 5:offset + 7]
     plot_distribution(axes, X, y, hist_nbins=50,
-                      plot_title=title + "\n(including outliers)",
+                      plot_title=title + " including outliers\n",
                       X_label="Median Income", y_label="Number of households")
 
+    # Some blank vertical space between two plots so they don't overlap
+    subplots[offset + 2].axis('off')
+    subplots[offset + 7].axis('off')
+
+    # Distribution with extreme outliers removed
     X0_min, X0_99th_pc = np.percentile(X[:, 0], [0, 99])
     X1_min, X1_99th_pc = np.percentile(X[:, 1], [0, 99])
 
     non_outliers = np.all(X < [X0_99th_pc, X1_99th_pc], axis=1)
-    axes = subplots[offset + 2: offset + 4] + subplots[offset + 6: offset + 8]
+    axes = subplots[offset + 3:offset + 5] + subplots[offset + 8:offset + 10]
     plot_distribution(axes, X[non_outliers], y[non_outliers], hist_nbins=50,
                       plot_title=(title +
-                                  "\n(Zoomed-in at quantile range [0, 99))"),
+                                  "\nZoomed-in at percentile range [0, 99)"),
                       X_label="Median Income", y_label="Number of households")
 
-    # Plot a heatmap legend for the y, combining a row of 4 cols
-    heatmap_legend_ax = plt.subplot(gs[offset+8: offset+12])
-    norm = mpl.colors.Normalize(y_full.min(), y_full.max())
-    mpl.colorbar.ColorbarBase(heatmap_legend_ax, cmap=cm.plasma_r,
-                              norm=norm, orientation='horizontal',
-                              label='color mapping for values of y')
-
-    # Blank space to avoid overlapping of plots;
-    # plt.tight_layout does not work with gridspec, height of this row is
-    # adjusted at `height_ratios` param given to `GridSpec`
-    plt.subplot(gs[offset+12: offset+16]).axis('off')
-
+# Plot a heatmap legend for the y, combining a row of 4 cols
+heatmap_legend_ax = plt.subplot(gs[-5:])
+norm = mpl.colors.Normalize(y_full.min(), y_full.max())
+mpl.colorbar.ColorbarBase(heatmap_legend_ax, cmap=cm.plasma_r,
+                          norm=norm, orientation='horizontal',
+                          label='Color mapping for values of y')
 plt.show()
