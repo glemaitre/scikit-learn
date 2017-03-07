70c35ec484c8efd8aa12d88010846c9f51e7a5f4
Raghav RV <rvraghav93@gmail.com>
2017-03-07 22:14:11 +0100

    FIX criterion must be a pyx file...

--- sklearn/regression_tree/criterion.pyx
+++ sklearn/regression_tree/criterion.pyx
@@ -1,7 +1,21 @@
-from __future__ import division, print_function
+# cython: cdivision=True
+# cython: boundscheck=False
+# cython: wraparound=False
 
+cimport numpy as np
 
-def _impurity_mse(stats_node):
+ctypedef np.npy_float64 DOUBLE_t         # Type of y, sample_weight
+ctypedef np.npy_intp SIZE_t              # Type for indices and counters
+
+from .stats_node cimport StatsNode
+from .split_record cimport SplitRecord
+
+cdef SIZE_t MSE_CRITERION = 0
+
+cpdef _impurity_mse_py(stats_node):
+    return _impurity_mse(stats_node)
+
+cdef inline DOUBLE_t _impurity_mse(StatsNode stats_node) nogil except -1:
     """Compute the impurity in MSE sense
 
     Parameters
@@ -14,16 +28,18 @@ def _impurity_mse(stats_node):
     impurity: float,
         The impurity.
     """
+    cdef DOUBLE_t impurity
     impurity = (stats_node.sum_sq_y /
                 stats_node.sum_weighted_samples)
     impurity -= ((stats_node.sum_y /
                   stats_node.sum_weighted_samples) ** 2.0)
-
     return impurity
 
 
-def impurity_improvement(c_split_record, sum_total_weighted_samples,
-                         criterion='mse'):
+cdef inline DOUBLE_t impurity_improvement(
+        SplitRecord c_split_record,
+        DOUBLE_t sum_total_weighted_samples,
+        SIZE_t criterion=MSE_CRITERION) nogil except -1:
     """Compute the impurity improvement.
 
     Parameters
@@ -42,11 +58,6 @@ def impurity_improvement(c_split_record, sum_total_weighted_samples,
     impurity_improvement: float,
         Impurity improvement
     """
-    if criterion == 'mse':
-        _impurity_func = _impurity_mse
-    else:
-        raise NotImplementedError
-
     # check that there is more sample in the root nodes than
     # in the current nodes
     if ((sum_total_weighted_samples <
@@ -55,15 +66,12 @@ def impurity_improvement(c_split_record, sum_total_weighted_samples,
          c_split_record.l_stats.sum_weighted_samples) or
         (sum_total_weighted_samples <
          c_split_record.r_stats.sum_weighted_samples)):
-        raise ValueError('The number of samples in the root nodes is'
-                         ' less than in the current children node.')
+        return -1
 
-    # impurity current node
-    c_impurity = _impurity_func(c_split_record.c_stats)
-    # impurity left child
-    l_impurity = _impurity_func(c_split_record.l_stats)
-    # impurity right child
-    r_impurity = _impurity_func(c_split_record.r_stats)
+    # impurity current node, left child and right child
+    cdef DOUBLE_t c_impurity = _impurity_mse(c_split_record.c_stats)
+    cdef DOUBLE_t l_impurity = _impurity_mse(c_split_record.l_stats)
+    cdef DOUBLE_t r_impurity = _impurity_mse(c_split_record.r_stats)
 
     return ((c_split_record.c_stats.sum_weighted_samples /
              sum_total_weighted_samples) *
